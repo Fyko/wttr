cefea828e229523a18506097a51a4a3520e8d3ee
Carter Himmel <carter@elhnet.net>
2020-07-31 17:15:22 -0600

    feat: remove tokio and use blocking

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate clap;
 use clap::App;
-use reqwest::{header, Client};
+use reqwest::{blocking, header};
 
 /// The type representing a URl
 type Url = &'static str;
@@ -13,8 +13,7 @@ const LONG_URL: Url = "https://wttr.in/";
 // The URL for version 2
 const PRETTY_URL: Url = "https://v2.wttr.in/";
 
-#[tokio::main]
-async fn main() {
+fn main() {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml)
         .version(crate_version!())
@@ -29,11 +28,10 @@ async fn main() {
         LONG_URL
     };
 
-    let response = Client::new()
+    let response = blocking::Client::new()
         .get(url)
         .header(header::USER_AGENT, "curl")
-        .send()
-        .await;
+        .send();
     let response = match response {
         Ok(res) => res,
         Err(err) => panic!(
@@ -42,7 +40,7 @@ async fn main() {
         ),
     };
 
-    let text = match response.text().await {
+    let text = match response.text() {
         Ok(text) => text,
         Err(err) => panic!(
             "An error occurred when trying to parse the response: {:#?}",
